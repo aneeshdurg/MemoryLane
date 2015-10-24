fd2e64e7bf9124774a7fb5ef695ca940c3fa708c
aneeshdurg <durg2@illinois.edu>
2015-10-24 18:30:37 -0500

    Updated static, some work with the login

--- memorylane/memorylane/settings.py
+++ memorylane/memorylane/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/1.8/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.8/ref/settings/
 """
+#import django.contrib.auth
+# django.contrib.auth.LOGIN_URL = '/'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
--- memorylane/memorylane/views.py
+++ memorylane/memorylane/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import get_object_or_404, render
 from django.template import Template, Context
 from django.http import HttpResponse
-
+from .forms import *
 
 from .models import User, Memory
+
+
+from django.contrib.auth import logout
+from django.views.decorators.csrf import csrf_protect
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
 
 def profiletest(request, user_id):
 	u = User.objects.get(pk=user_id)
@@ -38,3 +45,40 @@ def login(request):
 
 def friends(request):
 	return render(request, 'friends.html', {})
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+            username=form.cleaned_data['username'],
+            password=form.cleaned_data['password1'],
+            email=form.cleaned_data['email']
+            )
+            return HttpResponseRedirect('/register/success/')
+    else:
+        form = RegistrationForm()
+    variables = RequestContext(request, {
+    'form': form
+    })
+ 
+    return render_to_response(
+    'registration/register.html',
+    variables,
+    )
+ 
+def register_success(request):
+    return render_to_response(
+    'registration/success.html',
+    )
+ 
+def logout_page(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+ 
+#@login_required
+def home(request):
+    return render_to_response(
+    'home.html',
+    { 'user': request.user }
+    )	
