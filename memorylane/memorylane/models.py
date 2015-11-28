e13a8f8ae01e6e7282b22fdf3a38ac595b57ff6a
aneeshdurg <durg2@illinois.edu>
2015-11-28 15:12:35 -0600

    Changed user model, profilemod doesn't actually save bio yet. Author now records username

--- memorylane/memorylane/models.py
+++ memorylane/memorylane/models.py
@@ -1,26 +1,22 @@
 from django.db import models
 
-class User(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    username = models.CharField(max_length=50)
+class UserProfile(models.Model):
+    username = models.CharField(max_length=5000) 
     friends = models.CharField(max_length=5000)
     propic = models.CharField(max_length=1000)
     date_created = models.DateField()
-    email = models.EmailField()
-    password = models.CharField(max_length=100)
     bio = models.TextField()
     memories = models.CharField(max_length=5000)
-
+    
     def __str__(self):
-    	return self.first_name + " " + self.last_name
+        return self.first_name + " " + self.last_name
 
 class Memory(models.Model):
     name = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
     image = models.FileField(upload_to="memorylane/static/images")
     description = models.TextField()
-    author = models.IntegerField()
+    author = models.CharField(max_length=100)
     date_created = models.DateField()
 
     def __str__(self):
--- memorylane/memorylane/views.py
+++ memorylane/memorylane/views.py
@@ -3,9 +3,14 @@ from django.template import Template, Context
 from django.http import HttpResponse
 from .forms import *
 from datetime import datetime
-from .models import User, Memory
+from .models import Memory
+from .models import UserProfile
 
-from django.contrib.auth import logout
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as a_login
+from django.contrib.auth import logout as a_logout
+
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
@@ -18,21 +23,29 @@ def profiletest(request, user_id):
 
 def userlist(request):
 	lastest_user_list = User.objects.order_by('pk')[:5]
-	output = ', '.join([u.first_name for u in lastest_user_list])
+	output = ', '.join([u.username+' '+u.email for u in lastest_user_list])
 	return HttpResponse(output)
 
 def signup(request):
-    register(request)
-	
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
-        if form.is_valid():
-            user = User.objects.create_user(
-            username=username,
-            password=password,
-            email=email
-            )
-            return HttpResponseRedirect('/signup/success/')
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username)
+        if user is None:
+            email = request.POST['email']
+            if form.is_valid():
+                user = User.objects.create_user(
+                username=username,
+                password=password,
+                email=email
+                )
+                user.save()
+                profile = UserProfile(username=username, date_created=datetime.now())
+                profile.save()
+                return HttpResponseRedirect('/timeline/')
+        else:
+            return render(request, 'signup.html', {})
     else:
         form = RegistrationForm()
     variables = RequestContext(request, {
@@ -45,20 +58,19 @@ def settings(request):
 
 def post(request, memory_id):
 	memory = get_object_or_404(Memory, pk=memory_id)
-	author = get_object_or_404(User, pk=memory.author)
+	author = get_object_or_404(User, username=memory.author)
 	return render(request, 'post.html', {'memory': memory, 'author': author, 'image' : memory.image.name[10:]})
 
 def newpost(request):
-    author = get_object_or_404(User, pk=1)
-    username = author.username
+    username = request.user.username
     return render(request, 'newpost.html', {"username": username})
 
 def newpostsubmit(request):
     if 'title' in request.POST:
-        m = Memory(name=request.POST['title'], author=1, location=request.POST['location'], date_created=datetime.now(), description=request.POST['note_text'], image=request.FILES['media'])
+        m = Memory(name=request.POST['title'], author=request.user.username, location=request.POST['location'], date_created=datetime.now(), description=request.POST['note_text'], image=request.FILES['media'])
         m.save()
         memory = get_object_or_404(Memory, pk=m.id)
-        author = get_object_or_404(User, pk=memory.author)
+        author = get_object_or_404(User, username=memory.author)
         return post(request, m.id)
         message = 'Successfully added a new memory'
     else:
@@ -70,9 +82,23 @@ def passwordreset(request):
 
 def login(request):
     if request.method == 'POST':
-        user = get_object_or_404(User, password=request.POST['password'], email=request.POST['email'])
-        return timeline(request, user)
+        email = request.POST['email']
+        password = request.POST['password']
+        user = get_object_or_404(User, email=email)
+        user = authenticate(username=user.username, password=password)
+        if user is not None:
+            if user.is_active:
+                a_login(request, user)
+        else:
+            return settings(request)        
+        # a_login(request, user)
+        return timeline(request)
+    
     return render(request, 'login.html', {})
+
+def logout(request):
+    a_logout(request)
+    return render(request, 'login.html', {})    
 
 def friends(request):
 	return render(request, 'friends.html', {})
@@ -83,42 +109,10 @@ def following(request):
 def follower(request):
     return render(request, 'follower.html', {})
 
-def register(request):
-    if request.method == 'POST':
-        users = User.objects.all()
-        for x in users:
-            if x.username==request.POST['username']:
-                return HttpResponse('That username is taken, please choose another one.')
-            if x.email==request.POST['email']:
-                return HttpResponse('That email is already in use.')    
-        user = User(username=request.POST['username'], password=request.POST['password'], email=request.POST['email'], date_created=datetime.now())
-        user.save()
-        return timeline(request, user)    
-    else:
-        return HttpResponseRedirect('/signup/')
-
-#@login_required
-def home(request):
-    return render_to_response('home.html',{'user': request.user })
-
-def timeline(request, currentuser):
-    author = get_object_or_404(User, pk=1)
-    memory = get_object_or_404(Memory, pk=1)
-    username = author.username
-    first_name = author.first_name
-    description = memory.description
-    location = memory.location
-    name = memory.name
-    image = memory.image
-    date_created = memory.date_created
-    users = User.objects.all()
-    memories = Memory.objects.all()
-    return render(request, 'timeline.html', {"memories": memories})
-
 def timeline(request):
     author = get_object_or_404(User, pk=1)
     memory = get_object_or_404(Memory, pk=1)
-    username = author.username
+    username = request.user.username
     first_name = author.first_name
     description = memory.description
     location = memory.location
@@ -130,10 +124,10 @@ def timeline(request):
     return render(request, 'timeline.html', {"memories": memories, "username": username})
 
 def profilemod(request):
-    author = get_object_or_404(User, pk=1)
+    author = get_object_or_404(User, username=request.user.username)
     memory = get_object_or_404(Memory, pk=1)
-    username = author.username
-    first_name = author.first_name
+    username = request.user.username
+    first_name = request.user.first_name
     description = memory.description
     location = memory.location
     name = memory.name
@@ -141,12 +135,15 @@ def profilemod(request):
     date_created = memory.date_created
     memories = Memory.objects.all()
     if request.method == 'POST':
+        bio = get_object_or_404(UserProfile, username=request.user.username).bio
         form = BioForm(request.POST)
+        bio = form.bioTextArea
         if form.is_valid():
             return HttpResponseRedirect('/Saved/')
+
     else:
         author = get_object_or_404(User, pk=1)
-        bio = author.bio
+        bio = get_object_or_404(UserProfile, username=request.user.username).bio
         return render(request, 'profile-mod.html', {"bio": bio, "memories": memories, "first_name" : first_name, "username": username, "description": description, "name": name, "location": location, "image": image, "date_created": date_created})
 
 def getUsers(request):
